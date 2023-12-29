6e05e6e75e56f47fde01a4706239b74c3bf2b262
whalerice <joyfulforest@gmail.com>
2023-12-29 19:11:12 +0900

    Slow url checker

--- main.go
+++ main.go
@@ -1,17 +1,49 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/learngo/accounts"
+	"net/http"
 )
 
+var errRequestFailed = errors.New("Request failed")
+
 func main()  {
-	account := accounts.NewAccount("nico")
-	account.Deposit(10)
-	fmt.Println(account)
-	// err := account.Withdraw(20)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
+	var results = map[string]string{}
+	urls := []string {
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+		
+	}
+
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+
+		if err != nil {
+			result = "Failed"
+		}
+		results[url] = result
+	}
+	
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+func hitURL(url string)  error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err)
+		return errRequestFailed
+	}
+	return nil
 }
